7467f575a94c08a923ba06a6226ce90ca296556d
mnao305 <naomasa305@gmail.com>
2020-03-29 00:01:15 +0900

    feat: 撮影ボタンとそれに対するイベントの追加

--- dist/popup/index.js
+++ dist/popup/index.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log('Hello world');
+/**
+ * もろもろのイベントハンドラー
+ */
+var eventHandler = function () {
+    var _a;
+    // 撮影ボタンのイベント
+    (_a = document.getElementById('screenshot-button')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', function () {
+        // TODO: browser actionでは直接ページの内容にアクセスできないっぽいので、Event Pageを使ってイベントを作る
+    });
+};
+/**
+ * メイン
+ */
+var main = function () {
+    eventHandler();
+};
+main();
--- src/popup/index.ts
+++ src/popup/index.ts
@@ -1,1 +1,18 @@
-console.log('Hello world')
+/**
+ * もろもろのイベントハンドラー
+ */
+const eventHandler = (): void => {
+  // 撮影ボタンのイベント
+  document.getElementById('screenshot-button')?.addEventListener('click', () => {
+    // TODO: browser actionでは直接ページの内容にアクセスできないっぽいので、Event Pageを使ってイベントを作る
+  })
+}
+
+/**
+ * メイン
+ */
+const main = (): void => {
+  eventHandler()
+}
+
+main()
