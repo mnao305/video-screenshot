ff6240a01a143807f150a2570e953fe7680e3a90
mnao305 <naomasa305@gmail.com>
2020-03-29 23:21:31 +0900

    build: ビルド設定の変更
    webpackを使用する感じにした

---
+++ webpack.config.ts
@@ -0,0 +1,54 @@
+import { ConfigurationFactory } from 'webpack'
+import path from 'path'
+import CopyWebpackPlugin from 'copy-webpack-plugin'
+import { version } from './package.json'
+
+const config: ConfigurationFactory = () => {
+  return {
+    mode: process.env.NODE_ENV as 'development' | 'production' | 'none' | undefined,
+    context: path.join(__dirname, '/src'),
+    entry: {
+      popup: './popup/index.ts',
+      options: './options/index.ts',
+      background: './background/index.ts'
+    },
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: '[name]/index.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /.ts$/,
+          use: 'ts-loader',
+          exclude: '/node_modules/'
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['ts', 'js']
+    },
+    plugins: [
+      new CopyWebpackPlugin([
+        { from: 'icons', to: 'icons', ignore: ['icon.xcf'] },
+        { from: 'popup/index.html', to: 'popup/index.html' },
+        { from: 'popup/style.css', to: 'popup/style.css' },
+        { from: 'options/index.html', to: 'options/index.html' },
+        { from: 'options/style.css', to: 'options/style.css' },
+        {
+          from: 'manifest.json',
+          to: 'manifest.json',
+          transform: (content) => {
+            const jsonContent = JSON.parse(content.toString())
+            jsonContent.version = version
+
+            return JSON.stringify(jsonContent, null, 2)
+          }
+        }
+        // { from: '_locales', to: '_locales' } //多言語化対応用
+      ])
+    ]
+  }
+}
+
+export default config
