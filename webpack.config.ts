9aed355619897806bcc3ea1705fd72db7d08cb37
mnao305 <naomasa305@gmail.com>
2020-04-26 02:57:43 +0900

    build: fix webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -27,7 +27,7 @@ const config: ConfigurationFactory = (_, args) => {
       ]
     },
     resolve: {
-      extensions: ['ts', 'js']
+      extensions: ['.ts', '.js']
     },
     plugins: [
       new CopyWebpackPlugin([
