4959253c80e94263fa43b0e991b84449a95ddbb6
mnao305 <mail@mnao305.com>
2020-11-18 10:12:32 +0900

    refactor: オプションはフォームタグを使う感じに変更

--- src/options/index.ts
+++ src/options/index.ts
@@ -4,19 +4,19 @@ export interface Options {
   useShortcut: boolean
 }
 
-const shortcutOption = document.getElementById('shortcut') as HTMLInputElement
+const optionsForm = document.getElementById('options') as HTMLFormElement
 
 // 初期読み込み時に設定済みのオプション内容を反映させる
 ;(async function () {
   const option = await browser.storage.local.get({ useShortcut: true }) as Options
-  shortcutOption.checked = option.useShortcut
+  optionsForm.shortcut.checked = option.useShortcut
 }())
 
 // ショートカットキーでスクリーンショットを撮るかのオプション
-shortcutOption?.addEventListener('change', (e) => {
+optionsForm.addEventListener('change', () => {
   browser.storage.local.set(
     {
-      useShortcut: (e.target as HTMLInputElement).checked
+      useShortcut: optionsForm.shortcut.checked
     }
   ).catch(e => {
     console.error(e)
